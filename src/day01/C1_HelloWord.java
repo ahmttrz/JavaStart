6f0b69e67a8bf65e254aed3a9cb5a8d421b10466
ahmttrz <107430155+ahmttrz@users.noreply.github.com>
2022-06-13 22:37:14 +0200

    day01

---
+++ src/day01/C1_HelloWord.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class C1_HelloWord {
+	
+public static void main(String[] args) {
+	System.out.print("hakki baskan");
+	
+	System.out.println(15);
+	
+	System.out.print(false);
+}	
+
+}
